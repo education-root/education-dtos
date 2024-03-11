9afb094477642bd1bd3107481d0b542660f20240
bawawa <kobesmi@gmail.com>
2024-03-12 02:00:22 +0800

    2:57

--- src/admin/admin.ts
+++ src/admin/admin.ts
@@ -1,9 +1,14 @@
-
+import { ApiProperty } from '@nestjs/swagger';
 export class AdminInfoDto {
+    @ApiProperty()
     id?: string;
+    @ApiProperty()
     username: string="";
+    @ApiProperty()
     password: string="";
+    @ApiProperty()
     phone: string="";
+    @ApiProperty()
     role: "admin"|"teacher"|"head_teacher"="teacher"
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import {AdminInfoDto, LoginDto} from './admin/admin'
 
 
