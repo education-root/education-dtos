62506b3440aa6a32ebc43396eb95aed1b0048dc3
bawawa <kobesmi@gmail.com>
2024-03-12 11:13:52 +0800

    2:57

--- src/admin/admin.ts
+++ src/admin/admin.ts
@@ -10,3 +10,10 @@ export class LoginDto {
     username: string="";
     password: string="";
 }
+
+export class QueryConditionDto{
+    id?: string;
+    role?: "admin"|"teacher"|"head_teacher"
+    username?: string
+    phone?:string
+}
