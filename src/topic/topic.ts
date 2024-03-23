5cf73719a633a778b1ca14a8d606982cbe391ce6
bawawa <kobesmi@gmail.com>
2024-03-23 14:35:25 +0800

    subject

--- src/topic/topic.ts
+++ src/topic/topic.ts
@@ -12,6 +12,7 @@ export class TopicDto{
     type: 'single-choice' | 'multiple-choice' | 'true-false'="single-choice";
     analysis:string="";
     options: OptionDto[]=[];
+    relationUnit: string ="";
 }
 
 export class QueryTopicDto{
