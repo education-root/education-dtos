b590d23e8bcf7bfbb6a121e5d2470db5434ba835
bawawa <kobesmi@gmail.com>
2024-04-26 11:12:07 +0800

    stock

--- src/stock/stock.ts
+++ src/stock/stock.ts
@@ -1,11 +1,12 @@
 export class StockDto{
     id?:string;
     name: string=""
-    type: 1 | 2=1 // 1-练习题库， 2-模拟真题
+    type: 1 | 2 | 3=1 // 1-练习题库， 2-模拟真题， 3-ai批改
 }
 
 export class QueryStockDto{
     page: number=1;
     pageSize: number=10;
     name?: string;
+    type? : 1|2|3;
 }
